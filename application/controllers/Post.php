ac537792ce8135613623eae54c6c177cc9bee1ce
n-kishi108 <g1744366@cc.kyoto-su.ac.jp>
2019-03-12 15:05:35 +0900

    投稿機能を実装しました

---
+++ application/controllers/Post.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+class Post extends CI_Controller {
+	public $array;
+	public function __construct() {
+		parent::__construct();
+		$this->load->helper('url');
+		$this->array = array(
+			'id' => $_SESSION['id'],
+			'title' => $_POST['title'],
+			'head' => $_POST['head'],
+			'article' => $_POST['article']
+		);
+		$this->load->model('post_article');
+	}
+	public function index() {
+		$result = $this->post_article->post($this->array);
+		if($result) {
+			header('Location: '.base_url().'home/');
+			exit;
+		}else{
+			exit('投稿に失敗しました。');
+		}
+	}
+}
+ ?>
